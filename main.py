6395d0ab55afc74ad90eb2c1debabd7c1d577213
Markiyan Baziuk <markiyanbaziuk@knu.ua>
2026-04-10 11:17:19 +0300

    Створено точку входу в програму в main

--- main.py
+++ main.py
@@ -14,3 +14,15 @@ def save_results(data, output_filepath):
     with open(output_filepath, 'w', encoding='utf-8') as f:
         for word, count in data:
             f.write(f"{word}-{count}\n")
+
+def main():
+    """Основна логіка виконання програми."""
+    input_file = "input.txt"   
+    output_file = "output.txt" 
+    content = read_file(input_file)
+    top_words = get_top10_words(content)
+    save_results(top_words, output_file) 
+    print(f"10 найпопулярніших слів записано в {output_file}")
+
+if __name__ == "__main__":
+    main()
