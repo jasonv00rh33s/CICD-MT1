5dfe3627c56c0e1e5f627ca2995608df9991ab7a
Markiyan Baziuk <markiyanbaziuk@knu.ua>
2026-04-10 11:10:30 +0300

    Створення тесту для перевірки зчитування файлу

--- test_main.py
+++ test_main.py
@@ -1,5 +1,6 @@
 import pytest
-from main import get_top10_words, save_results
+import os
+from main import get_top10_words, save_results, read_file
 
 
 @pytest.fixture
@@ -20,3 +21,11 @@ def test_top_count(text):
     result = get_top10_words(text, top_n=2)
     assert len(result) == 2
     assert result[0] == ("apple", 3)
+
+def test_read_file(tmp_path):
+    d = tmp_path / "data"
+    d.mkdir()
+    file = d / "test.txt"
+    content = "hello world"
+    file.write_text(content, encoding='utf-8')
+    assert read_file(str(file)) == content
